6cd3b02839b2a81912b80646adb3519d90eadf2e
saifaldin3388 <saifaldin3388@gmail.com>
2025-07-15 20:53:29 +0300

    mainmain

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const socket = io('https://6d8b-156-218-13-163.ngrok.io', {
   transports: ['websocket'],
 });
 
-// Test 06
+// Test 08
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
